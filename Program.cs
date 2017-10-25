f2166ad327f7888077f556833e1a0cd526d75f78
Unknown <mati240311@gmail.com>
2017-10-25 19:40:51 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core_.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
